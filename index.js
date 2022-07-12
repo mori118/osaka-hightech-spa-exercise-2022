228d7459088225bbfd733017e47d704b9c6599a8
mori118 <gohiro1020@gmail.com>
2022-07-12 15:26:37 +0900

    hennkou

--- index.js
+++ index.js
@@ -2,53 +2,14 @@ const PrefectureCheckbox = {
   data() {
     return {
       prefectures: [
-        '北海道',
-        '青森県',
-        '岩手県',
-        '宮城県',
-        '秋田県',
-        '山形県',
-        '福島県',
-        '茨城県',
-        '栃木県',
-        '群馬県',
-        '埼玉県',
-        '千葉県',
-        '東京都',
-        '神奈川県',
-        '新潟県',
-        '富山県',
-        '石川県',
-        '福井県',
-        '山梨県',
-        '長野県',
-        '岐阜県',
-        '静岡県',
-        '愛知県',
-        '三重県',
-        '滋賀県',
-        '京都府',
-        '大阪府',
-        '兵庫県',
-        '奈良県',
-        '和歌山県',
-        '鳥取県',
-        '島根県',
-        '岡山県',
-        '広島県',
-        '山口県',
-        '徳島県',
-        '香川県',
-        '愛媛県',
-        '高知県',
-        '福岡県',
-        '佐賀県',
-        '長崎県',
-        '熊本県',
-        '大分県',
-        '宮崎県',
-        '鹿児島県',
-        '沖縄県',
+        '北海道','青森県','岩手県','宮城県','秋田県','山形県',
+        '福島県','茨城県','栃木県','群馬県','埼玉県','千葉県',
+        '東京都','神奈川県','新潟県','富山県','石川県','福井県',
+        '山梨県','長野県','岐阜県','静岡県','愛知県','三重県',
+        '滋賀県','京都府','大阪府','兵庫県','奈良県','和歌山県',
+        '鳥取県','島根県','岡山県','広島県','山口県','徳島県',
+        '香川県','愛媛県','高知県','福岡県','佐賀県','長崎県',
+        '熊本県','大分県','宮崎県','鹿児島県','沖縄県',
       ],
     };
   },
@@ -65,11 +26,13 @@ const PopulationBarPlot = {
   props: [ 'api' ],
   data() {
     return {
-      populations: [],
+      populations: [ 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 ],
+      result:'',
     };
   },
   /* html */
   template: `
+  <div>{{ result }}</div>
   <button v-on:click="updateGraph">更新</button>
   <div class="container">
     <div
@@ -79,20 +42,28 @@ const PopulationBarPlot = {
     ></div>
   </div>
   `,
-  methods: {
-    async updateGraph() {
+  methods:{
+    async updateGraph(){
       let xs = await getPopulations(this.api, 27);
 
-      // JSON から、'result' -> 'data' -> 0 番目 -> 'data'，と辿った箇所を xs に代入
+      // JSON から、'result' -> 'data' -> 0 番目 -> 'data'，と辿った箇所を hoge に代入
       xs = xs['result']['data'][0]['data'];
 
-      let ys = []
-
-      for (const x of xs) {
-        ys.push(x.value)
+      // xs を for 文で回し、中身の value をそれぞれ表示する
+      //let ys = []
+      //for(const x of xs){
+      //  ys.push(x.value);
+      // TODO: ↑の for 文で、数値の配列をうまく作り、this.result に代入する
+      //this.populations= ys;};
+      let values = xs.map(function(ys){
+        return ys["value"];
       }
-
-      this.populations = ys;
+      );
+      this.populations = values;
+      this.populations = this.populations.map((ys) => {
+        return ys / 20000;
+      }
+      );
     },
   },
 };
